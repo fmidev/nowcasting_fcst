4c6df723470a40155c5e324894a8726f81e52bda
Mikko Partio <mikko.partio@fmi.fi>
2018-10-29 13:46:09 +0200

    Use bitmap for grib writing

--- call_interpolation.py
+++ call_interpolation.py
@@ -172,6 +172,7 @@ def write_grib(interpolated_data,image_grib_file,write_grib_file,variable,predic
         for msg in grib:
             msg["generatingProcessIdentifier"] = 202
             msg["centre"] = 86
+            msg["bitmapPresent"] = True
 
             for i in range(interpolated_data.shape[0]):
                 msg["forecastTime"] = i

965952656ba1f34ce361665aeb8a6768a9bb13f1
Jussi Ylhäisi <jussi.ylhaisi@gmail.com>
2018-11-23 17:24:09 +0200

    encountered error in write_grib function

--- call_interpolation.py
+++ call_interpolation.py
@@ -183,6 +183,19 @@ def write_grib(interpolated_data,image_grib_file,write_grib_file,variable,predic
                     msg.write(out)
             break # we use only the first grib message as a template
 
+    ## This edits each grib message individually (and assumes that forecastTime is an integer value)
+    #with GribFile(image_grib_file) as grib:
+    #    for msg in grib:
+    #        msg["generatingProcessIdentifier"] = 202
+    #        msg["centre"] = 86
+    #        msg["bitmapPresent"] = True
+    #        i = msg["forecastTime"]
+    #        if (i == interpolated_data.shape[0]):
+    #            break
+    #        msg["values"] = interpolated_data[i,:,:].flatten()
+    #        with open(write_grib_file, "a") as out:
+    #            msg.write(out)
+
 
 
 
@@ -318,10 +331,10 @@ if __name__ == '__main__':
     #Parse commandline arguments
     parser = argparse.ArgumentParser()
     parser.add_argument('--obsdata',
-                        default="testdata/obsdata.grib2",
+                        default="testdata/obsdata_nomissing.grib2",
                         help='Obs data, representing the first time step used in image morphing.')
     parser.add_argument('--modeldata',
-                        default="testdata/modeldata.grib2",
+                        default="testdata/modeldata_nomissing.grib2",
                         help='Model data, from the analysis timestamp up until the end of the available 10-day forecast.')
     parser.add_argument('--seconds_between_steps',
                         type=int,
